29d1c499faeae486339997da5f71304cd772638b
blinkous <saradhani1@gmail.com>
2019-05-23 20:36:14 -0400

    can view and make offers on an artwork

--- Cassiopeia/MessagesViewController.swift
+++ Cassiopeia/MessagesViewController.swift
@@ -7,14 +7,92 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class MessagesViewController: UIViewController {
-
+class MessagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    // Create an empty array of PFObjects
+    var offers = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Refresh the view
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Offers")
+        
+        //        query.whereKey("art", equalTo:artid!)
+        
+        //                query.includeKeys(["artist", "art", "title"])
+        query.includeKey("buyer")
+        
+        query.findObjectsInBackground { (offers, error) in
+            // If you find offers, get them and reload the view
+            if offers != nil {
+                self.offers = offers!
+                self.tableView.reloadData()
+                print("there are offers for this work")
+                print(offers!)
+            }
+            else{
+                print("couldn't find any offers for this work")
+            }
+        }
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return offers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BuyerCell", for: indexPath) as! BuyerCell
+        
+        // Grab the data from the dictionary
+        let offer = offers[indexPath.row]
+        
+        let buyer = offer["buyer"] as! PFUser
+        
+        // Set the labels
+        cell.usernameLabel.text = buyer.username
+        cell.offerLabel.text = offer["offer"] as! String
+        
+        print("cell loaded")
+        
+        // Set the profile image
+        //        let pimageFile = buyer["image"] as! PFFileObject
+        //        let purlString = pimageFile.url!
+        //        let purl = URL(string: purlString)
+        //        cell.profileImageView.af_setImage(withURL: purl!)
+        
+        return cell
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//
+//        // Find the selected art
+//        let cell = sender as! UITableViewCell // cast the sender as the UITableViewCell
+//        let indexPath = tableView.indexPath(for: cell)! // tableview knows for a given cell what the index path is
+//        let artwork = artworks[indexPath.row] // finally access the array
+//        let user = artwork["artist"] as! PFUser
+//
+//        // Pass the selected art to the details view controller
+//        let detailsViewController = segue.destination as! MoreInfoViewController
+//        detailsViewController.artwork = artwork
+//        detailsViewController.user = user
+//        tableView.deselectRow(at: indexPath, animated: true) // deselect the row so that it's only highlighted briefly when you select it.
+//    }
     
 
     /*
--- Cassiopeia/MoreInfoViewController.swift
+++ Cassiopeia/MoreInfoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class MoreInfoViewController: UIViewController {
+class MoreInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var artistImageView: UIImageView!
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var artTitle: UILabel!
@@ -18,6 +18,11 @@ class MoreInfoViewController: UIViewController {
     @IBOutlet weak var mediumLabel: UILabel!
     @IBOutlet weak var dateMadeLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var tableVIew: UITableView!
+    
+    // Create an empty array of PFObjects
+    var offers = [PFObject]()
     
     var artwork: PFObject?
     var user = PFUser()
@@ -43,6 +48,90 @@ class MoreInfoViewController: UIViewController {
 //        let purlString = pimageFile.url!
 //        let purl = URL(string: purlString)
 //        artistImageView.af_setImage(withURL: purl!)
+        
+        tableVIew.delegate = self
+        tableVIew.dataSource = self
+        
+//        self.tableVIew.separatorStyle = UITableViewCell.SeparatorStyle.none
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Refresh the view
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Offers")
+        
+        let artid = artwork?.objectId
+        
+        // Trying to get a list of offers associated with this work
+        query.whereKey("artid", equalTo:artid!)
+        
+//        To bring up a list of all offers
+        query.includeKey("buyer")
+        
+        query.findObjectsInBackground { (offers, error) in
+            // If you find offers, get them and reload the view
+            if offers != nil {
+                self.offers = offers!
+                self.tableVIew.reloadData()
+                print("there are offers for this work")
+            }
+            else{
+                print("couldn't find any offers for this work")
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return offers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BuyerCell", for: indexPath) as! BuyerCell
+        
+        // Grab the data from the dictionary
+        let offer = offers[indexPath.row]
+        
+        let buyer = offer["buyer"] as! PFUser
+        
+        // Set the labels
+        cell.usernameLabel.text = buyer.username
+        cell.offerLabel.text = offer["offer"] as! String
+
+        print("cell loaded")
+        
+        // Set the profile image
+        //        let pimageFile = buyer["image"] as! PFFileObject
+        //        let purlString = pimageFile.url!
+        //        let purl = URL(string: purlString)
+        //        cell.profileImageView.af_setImage(withURL: purl!)
+        
+        return cell
+    }
+    
+    @IBAction func onTapNewOffer(_ sender: Any) {
+        // Create a PF Object that will be a table in the dashboard
+//        if artTitleField.text != nil && mediumField.text != nil && descField.text != nil && dateField.text != nil && artTitleField.text != "" && mediumField.text != "" && descField.text != "" && dateField.text != "" {
+        let offer = PFObject(className: "Offers")
+        
+        offer["buyer"] = PFUser.current()
+        offer["art"] = artwork
+        offer["seller"] = artwork!["artist"]
+        offer["artid"] = artwork?.objectId as! String
+        offer["offer"] = "125"
+        
+        offer.saveInBackground { (success, error) in
+            if success {
+                self.tableVIew.reloadData()
+                print("offer made")
+            } else {
+                print("offer NOT made")
+            }
+        }
+//        }
+//        else{
+//            print("offer not uploaded")
+//        }
     }
     
 
